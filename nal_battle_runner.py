39dd6e7ac1d33707b9bfe9a8af193207d24c9e31
waltersharpWEI <waltersharp@163.com>
2022-06-23 12:28:37 +0800

    the complex protocol runner

---
+++ nal_battle_runner.py
@@ -0,0 +1,10 @@
+
+import os
+from pathlib import Path
+
+if __name__=="__main__":
+    exp_path = "exp_output"
+    cmd_partial = "python nal_dropper.py"
+    cmd_udp = "python nal_dropper_udp.py"
+    cmd_quic = "python nal_dropper_quic.py"
+
